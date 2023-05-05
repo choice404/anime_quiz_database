3a024812185c468e57b1a2c73f0f3825a1f0aaf5
choisauce <austinch20@protonmail.com>
2023-05-05 13:58:58 -0700

    updated vue config public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/anime_quiz_database/" : "/",
   transpileDependencies: true
 })
